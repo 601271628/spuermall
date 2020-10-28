06e7e6abf4ca61fe26e87e1a63da4ef9f7a42dfd
Gitname <601271628@qq.com>
2020-10-29 00:44:29 +0800

    codewhy

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+
+//取文件别名
+module.exports={
+    configureWebpack:{
+        reslove:{
+            alias:{
+                'assets':'@/assets',
+                'common':'@/common',
+                'components':'@/components',
+                'network':'@/network',
+                'views':'@/views'
+            }
+        }
+    }
+}
