98fe1d658e5a790cbb83366f9af38b0405e561a9
Gitname <601271628@qq.com>
2020-10-29 22:50:58 +0800

    codewhy

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-
+    <router-view/>
+    <maintabbar></maintabbar>
   </div>
 </template>
 
 <script>
+import maintabbar from 'components/content/maintabbar/maintabbar'
   export default {
     name: 'App',
     components: {
-      
+      maintabbar
     }
   }
 </script>
 
 <style>
-  @import './assets/css/base.css'
+  @import './assets/css/base.css';
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const home =()=> import('views/home/home.vue')
+const category=()=>import ('views/category/category.vue')
+const cart=()=>import ('views/cart/cart.vue')
+const profile=()=>import ('views/profile/profile.vue')
+Vue.use(Router)
+
+
+const router= new Router({
+  mode:'history',
+  linkActiveClass:'active',
+  routes: [
+    {
+      path:'',
+      redirect:'/home'
+    },
+    {
+      path:'/home',
+      component:home,
+      meta:{
+        title:'首页'
+      }
+    },
+    {
+      path:'/category',
+      component:category,
+      meta:{
+        title:'分类'
+      }
+    },
+    {
+      path:'/cart',
+      component:cart,
+      meta:{
+        title:'购物车'
+      }
+    },
+    {
+      path:'/profile',
+      component:profile,
+      meta:{
+        title:'我的'
+      }
+    }
+  ]
+})
+
+router.beforeEach((to,from,next)=>{
+  document.title=to.meta.title
+  next() //不写这个next函数的话，路径都不出来（路径映射的组件没出来）
+})
+
+export default router
