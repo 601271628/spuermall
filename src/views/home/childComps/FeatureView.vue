1e45c63b5cd34458581d8d1093a21052009772eb
Gitname <601271628@qq.com>
2020-10-30 13:22:43 +0800

    codewhy

--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -2,20 +2,19 @@
 <template>
   <div class="feature">
     <a href="https://act.mogujie.com/zzlx64">
-      <img src="~assets/img/home/recommend.PNG" alt="">
+      <img src="~assets/img/home/recommend1.jpg" alt="">
     </a>
   </div>
 </template>
 
 <script>
 export default {
-  name:'FeatureView',
-  props:{
-
-  }
+  name:'FeatureView'
 }
 </script>
 
 <style  scoped>
-
+  .feature img{
+    width: 100%;
+  }
 </style>
