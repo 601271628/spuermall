139f971ac728420b1ec312d700d652b16995f703
Gitname <601271628@qq.com>
2020-11-08 10:49:38 +0800

    coderwhy

--- src/views/cart/cart.vue
+++ src/views/cart/cart.vue
@@ -4,21 +4,44 @@
     <nav-bar class="nav-bar">
       <div slot="center">购物车({{cartLength}})</div><!--购物车加入的不同的商品数-->
     </nav-bar>
+
+    <scroll class="cart-content" ref="scrollref">
+      <cart-list></cart-list>
+    </scroll>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import Scroll from 'components/common/scroll/Scroll'
+
+import {mapGetters} from 'vuex'
+
+import CartList from './childComps/CartList'
 
 export default {
   name:'cart',
   components:{
-    NavBar
+    NavBar,
+    CartList,
+    Scroll
   },
   computed:{
-    cartLength(){
-      return this.$store.getters.cartLength
-    }
+    //获取购物车数目
+    //方法1 cartLength(){return this.$store.state.cartList.length}
+
+    //方法2 cartLength(){return this.$store.getters.cartLength} 通过getter计算属性
+
+    //方法3 通过mapGetters辅助函数（把store的getters计算属性中的内容解析到这里的computed中
+    //用法1-数组形式 ...mapGetters(['cartLength','cartList'])
+    //用法2-对象形式
+    ...mapGetters({
+      cartLength:'cartLength',  //数组长度
+      cartList:'cartList'   //数组
+    })
+  },
+  activated(){
+    this.$refs.scrollref.refresh()
   }
 }
 </script>
@@ -29,5 +52,12 @@ export default {
     color:#fff;
     font-weight: bold;
     font-size: 20px;
+  }
+  .cart{
+    height: 100vh;
+  }
+  .cart-content{
+    height: calc(100% - 93px);
+    overflow: hidden;
   }
 </style>
